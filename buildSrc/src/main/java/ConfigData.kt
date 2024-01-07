ae31be6c776702d61a941c42b9e45a58aa2dcc9c
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-08 01:31:08 +0200

    better dependency management using buildSr

---
+++ buildSrc/src/main/java/ConfigData.kt
@@ -0,0 +1,14 @@
+import org.gradle.api.JavaVersion
+
+object ConfigData {
+    const val compileSdkVersion = 34
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 24
+    const val targetSdkVersion = 34
+    const val versionCode = 1
+    const val versionName = "1.0"
+    val JAVA_VERSIONS_CODE = JavaVersion.VERSION_17
+    const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+
+
+}
---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,9 @@
+object Modules {
+    const val DATA_REPO= ":data:repo"
+    const val DATA_REMOTE = ":data:remote"
+    const val DATA_LOCAL = ":data:local"
+    const val DOMAIN_USECASE = ":domain:usecase"
+    const val DOMAIN_MODEL= ":domain:models"
+    const val PRESENTATION_UI = ":Presentation:ui"
+    const val PRESENTATION_VIEWMODEL = ":Presentation:viewmodels"
+}
